2bfb21a1dc3bcefd3a5b6ba52054e68ca59d32d3
Max Balej <65896178+maxisusi@users.noreply.github.com>
2022-04-25 17:43:52 +0200

    Add all methods

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,14 +1,42 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
 
 @Controller('items')
 export class ItemsController {
+  // * Get all items
   @Get()
   findAll(): string {
     return 'Get all items';
   }
+  // * Get single item
+  @Get(':id')
+  findOne(@Param('id') id): string {
+    return `Item: ${id}`;
+  }
+
+  // * Create new item
   @Post()
   create(@Body() createItemDto: CreateItemDto): string {
-    return `Name: ${createItemDto.name} Desc: ${createItemDto.description} Qty: ${createItemDto.quantity}`;
+    return `Created - Name: ${createItemDto.name} Desc: ${createItemDto.description} Qty: ${createItemDto.quantity}`;
+  }
+
+  // * Delete Item
+  @Delete(':id')
+  delete(@Param('id') id): string {
+    return `Item Deleted: ${id}`;
+  }
+
+  // * Update Item
+  @Put(':id')
+  update(@Body() updateItemDto: CreateItemDto): string {
+    return `Uptated - Name: ${updateItemDto.name} Desc: ${updateItemDto.description} Qty: ${updateItemDto.quantity}`;
   }
 }
